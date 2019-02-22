e0930b3c8e6e44f99877f0f88435774ef7df515a
Robert Sheehan <robertnsheehan@gmail.com>
2019-02-22 15:43:49 +0000

    Update the EIM class to include virtual functions for the calculations done in the derived EIM classes. It means then that you can pass a pointer to a particular waveguide EIM calculation object and obviates the need for the dispersion class to have derived classes.

--- Dispersion/Dispersion/Dispersion_Calc.cpp
+++ Dispersion/Dispersion/Dispersion_Calc.cpp
@@ -5,6 +5,10 @@
 dispersion::dispersion()
 {
 	// Default Constructor
+	params_defined = false;
+
+	wl_N = 0; 
+
 	wl_low = wl_high = wl_delta = 0.0; 
 
 	core = nullptr; substrate = nullptr; cladding = nullptr; 
@@ -13,6 +17,15 @@ dispersion::dispersion()
 dispersion::dispersion(double wl_start, double wl_finish, double wl_step, material *Ncore, material *Nsub, material *Nclad)
 {
 	set_params(wl_start, wl_finish, wl_step, Ncore, Nsub, Nclad);
+}
+
+dispersion::~dispersion()
+{
+	// Deconstructor
+
+	params_defined = false; 
+
+	wl_vals.clear(); neff_vals.clear();	ng_vals.clear();
 }
 
 void dispersion::set_params(double wl_start, double wl_finish, double wl_step, material *Ncore, material *Nsub, material *Nclad)
@@ -27,7 +40,23 @@ void dispersion::set_params(double wl_start, double wl_finish, double wl_step, m
 		bool c6 = Nclad != nullptr ? true : false; 
 		bool c10 = c1 && c2 && c3 && c4 && c5 && c6; 
 
-		if (c10) {}
+		if (c10) {
+			wl_low = wl_start; 
+			wl_high = wl_finish; 
+			wl_delta = wl_step; 
+
+			wl_N = static_cast<int>(1.0 + ( (wl_high - wl_low) / wl_delta) ); 
+
+			core = Ncore; 
+			substrate = Nsub; 
+			cladding = Nclad; 
+
+			wl_vals.clear(); 
+			neff_vals.clear(); 
+			ng_vals.clear(); 
+
+			params_defined = true; 
+		}
 		else {
 			std::string reason; 
 			reason = "Error: void dispersion::set_params(double wl_start, double wl_finish, double wl_step, material *Ncore, material *Nsub, material *Nclad)\n"; 
@@ -40,6 +69,27 @@ void dispersion::set_params(double wl_start, double wl_finish, double wl_step, m
 			throw std::invalid_argument(reason); 
 		}
 
+	}
+	catch (std::invalid_argument &e) {
+		useful_funcs::exit_failure_output(e.what());
+		exit(EXIT_FAILURE);
+	}
+}
+
+void dispersion::compute_dispersion_data()
+{
+	// Compute the dispersion data based on the defined inputs
+
+	try {
+		if (params_defined) {
+			// Compute dispersion curves here
+			
+		}
+		else {
+			std::string reason;
+			reason = "Error: void dispersion::compute_dispersion_data()\n";
+			throw std::invalid_argument(reason);
+		}
 	}
 	catch (std::invalid_argument &e) {
 		useful_funcs::exit_failure_output(e.what());
--- Dispersion/Dispersion/Dispersion_Calc.h
+++ Dispersion/Dispersion/Dispersion_Calc.h
@@ -6,16 +6,28 @@
 // given waveguide structure. Data is waveguide geometry and material dependent
 // R. Sheehan 22 - 2 - 2019
 
+// Update the EIM code to include virtual functions for reducing a wg and assigning parameters
+// That way you can make EIM object part of the dispersion class as a pointer to an object derived from EIM
+// It will make the coding much easier and obviate the need to have derived classes for the dispersion class
+// R. Sheehan 22 - 2 - 2019
+
 class dispersion {
 public:
 	dispersion(); 
 
 	dispersion(double wl_start, double wl_finish, double wl_step, material *Ncore, material *Nsub, material *Nclad);
 
+	~dispersion(); 
+
 	void set_params(double wl_start, double wl_finish, double wl_step, material *Ncore, material *Nsub, material *Nclad);
 
-private:
-	
+	void compute_dispersion_data();
+
+protected:
+	bool params_defined; 
+
+	int wl_N; // number of wl steps required for calculation
+
 	double wl_low; // lower bound on wavelength range, units of um
 	double wl_high; // upper bound on wavelength range, units of um 
 	double wl_delta; // wavelength step-size, units of um
@@ -23,6 +35,10 @@ private:
 	material *core; // object for the core material
 	material *substrate; // object for the substrate material
 	material *cladding; // object for the cladding material
+
+	std::vector<double> wl_vals; 
+	std::vector<double> neff_vals; 
+	std::vector<double> ng_vals; 
 };
 
 #endif
